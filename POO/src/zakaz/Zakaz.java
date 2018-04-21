4e00a323d9475b2fe8678c5cb9d4d77ce340e139
graninty <31422278+graninty@users.noreply.github.com>
2018-04-21 15:15:15 -0300

    Update Zakaz.java

--- POO/src/zakaz/Zakaz.java
+++ POO/src/zakaz/Zakaz.java
@@ -3,16 +3,98 @@ package zakaz;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.Scanner;
+
 
 public class Zakaz
 {
     public static void main(String[] args) throws SQLException, IOException, ParseException
     {
-        Empresa empresa = new Empresa();
+        boolean terminar==false;
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Por favor ingrese el nombre de la empresa");
+        String nombreEmpresa= entrada.nextLine();
+        System.out.println("Por favor ingrese la direccion de la empresa");
+        String direccionEmpresa= entrada.nextLine();
+        System.out.println("Por favor ingrese el rut de la empresa");
+        String rutEmpresa= entrada.nextLine();
+        System.out.println("Por favor ingrese el numero telefonico de la empresa");
+        int telefonoEmpresa= entrada.nextInt();
+        
+        Empresa empresa = new Empresa(nombreEmpresa,
+                                     direccionEmpresa,
+                                     rutEmpresa,
+                                     telefonoEmpresa);
         
         empresa.cargar();
-        
         //Menu
+        while(terminar==false){
+            System.out.println("Bienvenido al gestor de Planillas Zakaz\n\n");
+            System.out.println("menu\n");
+            System.out.println("1- agregar encargado\n");
+            System.out.println("2- agregar producto\n");
+            System.out.println("3- agregar vehiculo\n");
+            System.out.println("4- agregar planilla\n");
+            
+            System.out.println("2- cambiar encargado\n");
+            
+            System.out.println("ingrese una opcion\n");
+            int op= entrada.nextInt();
+            
+            if (op==1){
+                System.out.println("ingrese nombre\n");
+                String nombreEncargado= entrada.nextLine();
+                System.out.println("ingrese año nacimiento\n");
+                int añoEncargado= entrada.nextInt();
+                System.out.println("ingrese rut\n");
+                String rutEncargado= entrada.nextLine();
+                
+                if (agregarEncargado(nombreEncargado,
+                                     añoEncargado,
+                                     rutEncargado)==true){
+                    System.out.println("encargado ingresado exitosamente\n");
+                }
+                else{
+                    System.out.println("error al ingresar encargado\n");
+                }
+            if (op==2){
+                System.out.println("ingrese el nombre del producto\n");
+                String nombreProducto= entrada.nextLine();
+                System.out.println("ingrese una id para el producto\n");
+                int idProducto= entrada.nextInt();
+                System.out.println("ingrese su valor\n");
+                int valorProducto= entrada.nextInt();
+                
+                if (agregarProducto(nombreProducto,
+                                    idProducto,
+                                    valorProducto)==true){
+                        System.out.println("producto ingresado exitosamente\n");
+                    }
+                else{
+                        System.out.println("error al ingresar producto\n");
+                }
+            if (op==3){
+                System.out.println("ingrese la marca del vehiculo\n");
+                String marcaVehiculo= entrada.nextLine();
+                System.out.println("ingrese tipo del vehiculo\n");
+                String tipoVehiculo= entrada.nextLine();
+                System.out.println("ingrese modelo del vehiculo\n");
+                String modeloVehiculo= entrada.nextLine();
+                System.out.println("ingrese patente del vehiculo\n");
+                String patenteVehiculo= entrada.nextLine();
+                
+                if (agregarVehiculo(marcaVehiculo,
+                                    tipoVehiculo,
+                                    modeloVehiculo,
+                                    patenteVehiculo)==true){
+                        System.out.println("vehiculo ingresado exitosamente\n");
+                    }
+                else{
+                        System.out.println("error al ingresar vehiculo\n");
+                }
+        }
+        //Menu?
         empresa.empleados.mostrarEmpleados();
         System.out.println(empresa.locales.mostrarLocales());
         empresa.productos.mostrarProductos();
